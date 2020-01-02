fc5f6ec1b613941e2f70e08c59d2994bace0a356
IvanDSElite <57435548+IvanDSElite@users.noreply.github.com>
2020-01-02 12:44:42 +0100

    Add files via upload

---
+++ gamelib.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+struct Scavatrice {
+  struct Caverna *posizione;
+  int serbatoio_energia;    //valore massimo 4
+  int serbatoio_raccolta;   //valore massimo 10
+  enum stato {vivo, morto, uscito} stato;
+};
+
+struct Caverna {
+  int a;
+  struct Caverna *avanti;
+  struct Caverna *destra;
+  struct Caverna *sinistra;
+  enum melassa {nessuna, poca, molta = 3} melassa;
+  enum imprevisto {nessun_imprevisto, crollo, baco} imprevisto;
+  enum tipo_caverna {normale, speciale, accidentata, uscita} tipo_caverna;
+};
